066c4c1773385c1e4831383ad7a623f6f251e862
dizzie <dizziefalls@gmail.com>
2023-03-02 16:36:13 -0600

    Oops big commit, splits canvas, creates rendering engine, many others

--- src/grid/grid.ts
+++ src/grid/grid.ts
@@ -1,8 +1,9 @@
-import { Entity } from "@/utils";
+import { Entity, Vector2D } from "@/utils";
 import { Node } from "@/node";
+import { Settings } from "@/settings";
 
 export class Grid extends Entity {
-  private _nodes: Entity[] = []
+  private _nodes: Node[] = []
 
   public get Nodes(): Node[] {
     return this._nodes
@@ -10,6 +11,8 @@ export class Grid extends Entity {
 
   public Awake(): void {
     super.Awake() // components
+
+    this.InitNodes()
 
     for (const node of this._nodes) { // children
       node.Awake()
@@ -21,6 +24,29 @@ export class Grid extends Entity {
 
     for (const node of this._nodes) {
       node.Update(deltaTime)
+    }
+  }
+
+  private InitNodes(): void {
+    const size = Settings.grid.nodeSize
+    const offset = Settings.grid.nodeOffset
+    for (let y = 0; y < Settings.grid.dimension; y++) {
+      for (let x = 0; x < Settings.grid.dimension; x++) {
+        const start = new Vector2D(
+          x * (size + offset) + offset,
+          y * (size + offset) + offset
+        )
+
+        const end = new Vector2D(
+          start.x + size,
+          start.y + size
+        )
+
+        const index = new Vector2D(x, y)
+
+        const node = new Node(start, end, index)
+        this._nodes.push(node)
+      }
     }
   }
 }
--- src/node/components/draw/draw.ts
+++ src/node/components/draw/draw.ts
@@ -1,14 +1,32 @@
 import { IComponent } from "@/utils";
 import { Node } from "@/node/node";
+import { Settings } from "@/settings";
+import { CanvasLayer } from "@/canvas-layer";
 
 export class NodeDrawComponent implements IComponent {
   public Entity: Node
 
   public Awake(): void {
-    
+    this.Clear()
   }
 
   public Update(deltaTime: number): void {
-    
+    this.Clear()
+    this.Draw() 
+  }
+
+  private Draw(): void {
+    CanvasLayer.Background.FillRect(
+      this.Entity.Start,
+      this.Entity.Size,
+      Settings.grid.color
+    )
+  }
+
+  private Clear(): void {
+    CanvasLayer.Background.ClearRect(
+      this.Entity.Start,
+      this.Entity.Size
+    )
   }
 }
