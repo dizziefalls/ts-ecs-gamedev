8b5e1651e79656a85c3fdda0c217596e450217e3
dizzie <dizziefalls@gmail.com>
2023-03-02 17:29:08 -0600

    Creates color system, canvas layers

--- src/utils/canvas/Canvas.ts
+++ src/utils/canvas/Canvas.ts
@@ -1,4 +1,4 @@
-import { IAwake, Vector2D } from '@/utils'
+import { Color, IAwake, Vector2D } from '@/utils'
 
 export class Canvas implements IAwake {
   private _elm: HTMLCanvasElement
@@ -30,14 +30,36 @@ export class Canvas implements IAwake {
     this._ctx = ctx
   }
 
-  public FillRect(start: Vector2D, size: Vector2D, color: string): void {
+  //Partial allows for only a few options
+  public SetStyle(style: Partial<CSSStyleDeclaration>): void {
+    for (const key in style) {
+      if (!Object.hasOwnProperty.call(style, key)) {
+        continue
+      }
+
+      if (!style[key]) {
+        continue
+      }
+
+      this._elm.style[key] = style[key] as string
+    }
+  }
+
+  public FillRect(start: Vector2D, size: Vector2D, color: Color): void {
     this._ctx.beginPath()
-    this._ctx.fillStyle = color
+    this._ctx.fillStyle = color.AsString()
     this._ctx.rect(start.x, start.y, size.x, size.y)
     this._ctx.fill()
   }
 
   public ClearRect(start: Vector2D, size: Vector2D): void {
     this._ctx.clearRect(start.x, start.y, size.x, size.y)
+  }
+
+  public FillCircle(center: Vector2D, radius: number, color: Color): void {
+    this._ctx.beginPath()
+    this._ctx.arc(center.x, center.y, radius, 0, Math.PI * 2)
+    this._ctx.fillStyle = color.AsString()
+    this._ctx.fill()
   }
 }
